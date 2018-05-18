3cac8949c3dbc2fe26f5e7e8e7da1fa99c45cdc3
Eric Leroy-Terquem <eric@captive.fr>
2018-05-18 21:17:16 +0200

    permet a un visiteur de s'inscrire. Il a un nom, un mot de passe et un role par defaut. Je peux créer un admin en console

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_18_190540) do
+ActiveRecord::Schema.define(version: 2018_05_18_190835) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", default: "simple user"
   end
 
 end
