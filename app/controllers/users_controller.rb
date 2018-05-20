4ea2b9ec7f2e3c0ecf0c60548eadea08d3eb4e23
Eric Leroy-Terquem <eric@captive.fr>
2018-05-20 14:09:17 +0200

    refactore le set_user dans le user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user
+
   def login
   end
   
@@ -29,16 +30,13 @@ class UsersController < ApplicationController
   end
   
   # GET /users
-  # GET /users.json
   def index
     @users = User.all
     @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 
   # GET /users/1
-  # GET /users/1.json
   def show
-    set_user
   end
 
   # GET /users/new
@@ -48,11 +46,9 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    set_user
   end
 
   # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -68,24 +64,18 @@ class UsersController < ApplicationController
   end
 
   # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
-    set_user
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
-    set_user
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
@@ -96,7 +86,9 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      if params[:id]
+        @user = User.find(params[:id])
+      end
     end
 
   # Never trust parameters from the scary internet, only allow the white list through.
