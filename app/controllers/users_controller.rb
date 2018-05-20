07332120a12fea58f052ef2337e3ab550b1bd425
Eric Leroy-Terquem <eric@captive.fr>
2018-05-20 14:00:00 +0200

    permet la connexion et la deconnexion de l'utilisateur

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,44 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
+  def login
+  end
+  
+  def logout
+    session[:user_id] = nil
+    flash[:info] = "Vous êtes maintenant déconnecté"
+    redirect_to "/users"
+  end
 
+  def check
+    @current_user = User.where(name: params[:name], password: params[:password]).first
+    p '#'*30
+    p @current_user.name
+    
+    p '#'*30
+    if @current_user
+      session[:user_id] = @current_user.id
+      p '*'*50
+      p session[:user_id]
+      flash[:info] = "You are now connected"
+      redirect_to "/users"
+    else
+      session[:user_id] = nil
+      flash[:info] = "You are now disconnected"
+      redirect_to "/users/login"
+    end
+  end
+  
   # GET /users
   # GET /users.json
   def index
     @users = User.all
+    @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
+    set_user
   end
 
   # GET /users/new
@@ -19,6 +48,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    set_user
   end
 
   # POST /users
@@ -40,6 +70,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    set_user
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
@@ -54,6 +85,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    set_user
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
@@ -67,7 +99,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+  # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :password)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/users/login', to: 'users#login'
+  get '/users/logout', to: 'users#logout'
+  post '/users/login', to: 'users#check'
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "users#index"
+
 end
