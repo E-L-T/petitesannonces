605554e232032625d0f62ed6ba504cf96fbdff2c
Eric Leroy-Terquem <eric@captive.fr>
2018-06-13 22:41:26 +0200

    ajoute la gem launchy pour ouvrir automatiquement la page html lors d'un save_and_open_page

--- Gemfile
+++ Gemfile
@@ -58,6 +58,8 @@ group :test do
   gem 'chromedriver-helper'
   gem 'factory_bot_rails'
   gem 'shoulda-matchers', '~> 3.1'
+  gem 'launchy'
+
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -4,11 +4,12 @@ require 'rails_helper'
 
 describe "Users", type: :feature do
 
-  scenario "create an user", js: true do
+  scenario "create an user", :focus do
     visit new_user_path
     fill_in :user_name, with: 'Roger'
     fill_in :user_password, with: 'password'
     click_on 'Create User'
+    save_and_open_page
 
     expect(page).to have_content 'Roger'
   end
